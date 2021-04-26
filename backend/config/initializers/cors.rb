5bdd6dfccddb40babfde502c9430f599af660baa
Alisson Tenório Pinto <alissonal@gmail.com>
2021-04-25 21:23:52 -0300

    modificado db

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins 'andidatosgh.herokuapp.com'
+     origins 'candidatosgh.herokuapp.com'
 
      resource '*',
        headers: :any,
